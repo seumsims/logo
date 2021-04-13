a63f1dfe242609ad8c0f44a46b7bdddc588d3736
Tester <seumworldinbangladesh@gmail.com>
2021-04-13 15:58:13 +0600

    after_theme_setup and after_setup_theme are different functions

--- functions.php
+++ functions.php
@@ -1,4 +1,8 @@
-<?php 
-$title = get_bloginfo("name");
-_e(strtoupper($title));
-require "postloop.php";
+<?php
+function logo_bootstrapping() {
+    load_theme_textdomain("logo");
+    add_theme_support("post-thumbnails");
+    add_theme_support("title-tag");
+
+}
+add_action("after_theme_setup","logo_bootstrapping");
