184dc20011607a614998644751b5733afdd41588
Tester <seumworldinbangladesh@gmail.com>
2021-04-15 17:02:04 +0600

    corrected sort with home

---
+++ home-bak-1.php
@@ -0,0 +1,15 @@
+<?php wp_head(); ?>
+<?php 
+//the_content();
+/*
+while(have_posts()) {
+
+    the_post();
+    _e("<h1>");
+    the_title();
+    _e("</h1>");
+
+} 
+*/
+?>
+<?php wp_footer(); ?>
---
+++ single.php
@@ -0,0 +1,14 @@
+<?php wp_head(); ?>
+<?php 
+/*
+while(have_posts()) {
+
+    the_post();
+    _e("<h1>");
+    the_title();
+    _e("</h1>");
+
+} 
+*/
+?>
+<?php wp_footer(); ?>
