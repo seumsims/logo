000a8bba394c555962684ac772b2139067200f90
Tester <seumworldinbangladesh@gmail.com>
2021-04-18 20:30:23 +0600

    success to set post thumbnail

--- page.php
+++ page.php
@@ -2,6 +2,7 @@
 <?php 
 _e("this is page");
 the_content();
+//the_post_thumbnail( );
 ?>
 
 <?php wp_footer(); ?>
---
+++ single-logo.php
@@ -0,0 +1,19 @@
+<?php wp_head(); ?>
+<?php 
+_e("this is single logo ");
+_e(" <br> ");
+while(have_posts()) {
+
+    the_post();
+    _e("<h1 style=\"color:green\">");
+    the_title();
+    _e("</h1>");
+    the_post_thumbnail();
+    _e("<article>");
+    the_content();
+    _e("</article>");
+
+} 
+?>
+
+<?php wp_footer(); ?>
