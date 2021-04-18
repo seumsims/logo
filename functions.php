7f44474a49b2fc7bd3845c96043d533a84127085
Tester <seumworldinbangladesh@gmail.com>
2021-04-18 19:50:19 +0600

    set category tax and labeled it

--- functions.php
+++ functions.php
@@ -126,7 +126,11 @@ function logo_one_page_post_type_custom_taxonomy_types() {
 
     $args = [
         
-
+        'labels' => [
+            'name' => 'Logo Types',
+            'singular_name' => 'Type',
+        ],
+        
         'public' => true,
         'hierarchical' => true
 
