394c76417459ae036737bf4982dfcb52e067369f
Tester <seumworldinbangladesh@gmail.com>
2021-04-19 21:10:13 +0600

    page text showed

--- functions.php
+++ functions.php
@@ -164,21 +164,23 @@ add_action("init", "new_post_type_for_one_page");
  * Register a custom menu page.
  */
 function wpdocs_register_my_custom_menu_page(){
+
+    /**
+ * Display a custom menu page
+ */
+function my_custom_menu_page(){
+    esc_html_e( 'Admin Logo Page Test', 'logo' );  
+}
     add_menu_page( 
-        __( 'Custom Menu Title', 'logo' ),
+        __( 'Logo Menu Name', 'logo' ),
         'Logo Menu',
         'manage_options',
         'custompage',
         'my_custom_menu_page',
-        plugins_url( 'screenshot.png' ),
+        $icon_url   = 'dashicons-desktop',
         10
     ); 
 }
 add_action( 'admin_menu', 'wpdocs_register_my_custom_menu_page' );
  
-/**
- * Display a custom menu page
- */
-function my_custom_menu_page(){
-    esc_html_e( 'Admin Page Test', 'logo' );  
-}
+
