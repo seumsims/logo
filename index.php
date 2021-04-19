f214cb5530eb44c083d1ca6eab000a5a655afd73
Tester <seumworldinbangladesh@gmail.com>
2021-04-19 16:23:48 +0600

    some set

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
-<?php wp_head(); ?>
-
 <?php 
-//the_content();
-
+esc_html_e( basename(get_page_template(  ))  );
 ?>
-
-
-<?php wp_footer(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo( 'name' ); ?></title>
+    <?php wp_head(  ); ?>
+</head>
+<body <?php body_class(  )?>>
+<?php the_title( ); ?>
+<?php the_content(); ?>
+<?php wp_footer(  ) ?>   
+</body>
+</html>
