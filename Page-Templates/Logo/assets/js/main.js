8664cea6aef11db761a966c608d3a2aad3b75436
Tester <seumworldinbangladesh@gmail.com>
2021-04-19 16:34:04 +0600

    page templates not visible

---
+++ Page-Templates/Logo/assets/js/main.js
@@ -0,0 +1,248 @@
+/*---------------------------------------------
+Template name:  tf-quickstart || html template
+Version:        1.0
+Author:         rajibmehedihasan
+Author url:     https://github.com/rajibmehedihasan
+
+NOTE:
+------
+Please DO NOT EDIT THIS JS, you may need to use "custom.js" file for writing your custom js.
+We may release future updates so it will overwrite this file. it's better and safer to use "custom.js".
+
+[Table of Content]
+----------------------------------------------*/
+
+(function ($) {
+    "use strict";
+    // meanmenu
+
+$('#mobile-menu').meanmenu({
+
+	meanMenuContainer: '.hibo-mobile-menu',
+
+	meanScreenWidth: "1200",
+
+	onePage: true
+
+});
+    $("[data-background]").each(function () {
+
+        $(this).css("background-image", "url(" + $(this).attr("data-background") + ")")
+        })
+
+
+    // // owlCarousel
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:0,
+      items:1,
+      navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+      nav:true,
+      dots:false,
+      responsive:{
+          0:{
+              items:1
+          },
+          767:{
+              items:2
+          },
+          992:{
+              items:2
+          }
+      }
+  })
+
+  // isotop
+
+	$('.grid').imagesLoaded(function () {
+
+        // init Isotope
+    
+        var $grid = $('.grid').isotope({
+    
+          itemSelector: '.grid-item',
+    
+          percentPosition: true,
+    
+          masonry: {
+    
+            // use outer width of grid-sizer for columnWidth
+    
+            columnWidth: 1
+    
+          }
+    
+        });
+    
+    
+    
+        // filter items on button click
+    
+        $('.upcomming-isotop-manu').on('click', 'button', function () {
+    
+            var filterValue = $(this).attr('data-filter');
+    
+            $grid.isotope({ filter: filterValue });
+    
+        });
+    
+    
+    
+    });
+    
+    
+    
+    
+    
+    //for menu active class
+    
+    $('.upcomming-isotop-manu > button').on('click', function(event) {
+    
+        $(this).siblings('.active').removeClass('active');
+    
+        $(this).addClass('active');
+    
+        event.preventDefault();
+    
+    });
+
+
+
+
+
+
+    // // meanmenu
+    // $('#mobile-menu').meanmenu({
+    //     meanMenuContainer: '.mobile-menu',
+    //     meanScreenWidth: "992"
+    // });
+    
+    // // One Page Nav
+    // var top_offset = $('.header-area').height() - 10;
+    // $('.main-menu nav ul').onePageNav({
+    //     currentClass: 'active',
+    //     scrollOffset: top_offset,
+    // });
+    
+    
+    $(window).on('scroll', function () {
+        var scroll = $(window).scrollTop();
+        if (scroll < 245) {
+            $(".header-wrapper").removeClass("sticky");
+        } else {
+            $(".header-wrapper").addClass("sticky");
+        }
+    });
+    
+    
+    
+    // // mainSlider
+    // function mainSlider() {
+    //     var BasicSlider = $('.slider-active');
+    //     BasicSlider.on('init', function (e, slick) {
+    //         var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
+    //         doAnimations($firstAnimatingElements);
+    //     });
+    //     BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+    //         var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+    //         doAnimations($animatingElements);
+    //     });
+    //     BasicSlider.slick({
+    //         autoplay: false,
+    //         autoplaySpeed: 10000,
+    //         dots: false,
+    //         fade: true,
+    //         arrows: false,
+    //         responsive: [
+    //             { breakpoint: 767, settings: { dots: false, arrows: false } }
+    //         ]
+    //     });
+    
+    //     function doAnimations(elements) {
+    //         var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    //         elements.each(function () {
+    //             var $this = $(this);
+    //             var $animationDelay = $this.data('delay');
+    //             var $animationType = 'animated ' + $this.data('animation');
+    //             $this.css({
+    //                 'animation-delay': $animationDelay,
+    //                 '-webkit-animation-delay': $animationDelay
+    //             });
+    //             $this.addClass($animationType).one(animationEndEvents, function () {
+    //                 $this.removeClass($animationType);
+    //             });
+    //         });
+    //     }
+    // }
+    // mainSlider();
+    
+    
+    // // owlCarousel
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        items:1,
+        navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            767:{
+                items:1
+            },
+            992:{
+                items:1
+            }
+        }
+    })
+    
+    
+    // /* magnificPopup img view */
+    // $('.popup-image').magnificPopup({
+    //     type: 'image',
+    //     gallery: {
+    //       enabled: true
+    //     }
+    // });
+    
+    // /* magnificPopup video view */
+    // $('.popup-video').magnificPopup({
+    //     type: 'iframe'
+    // });
+    
+    
+
+    
+    
+    
+
+// scrollToTop
+
+$.scrollUp({
+
+	scrollName: 'scrollUp', // Element ID
+
+	topDistance: '300', // Distance from top before showing element (px)
+
+	topSpeed: 300, // Speed back to top (ms)
+
+	animation: 'fade', // Fade, slide, none
+
+	animationInSpeed: 200, // Animation in speed (ms)
+
+	animationOutSpeed: 200, // Animation out speed (ms)
+
+	scrollText: '<i class="fas fa-long-arrow-alt-up"></i>', // Text for element
+
+	activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+
+});
+
+
+    // // WOW active
+    // new WOW().init();
+    
+    
+    })(jQuery);
