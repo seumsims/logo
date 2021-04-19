5046bed2d62a89d653518dff0de06ea0f38b8ff3
Tester <seumworldinbangladesh@gmail.com>
2021-04-19 16:44:51 +0600

    get template part dont need ext

--- Page-Templates/Logo/index.htm.php
+++ Page-Templates/Logo/index.htm.php
@@ -1,8 +1,3 @@
-<?php
-/*
-Template Name: Logo
-*/
- ?>
 <!DOCTYPE html>
 <html lang="en">
 
---
+++ testtem.php
@@ -0,0 +1,6 @@
+<?php
+/*
+Template Name: Logo
+*/
+ ?>
+ <?php get_template_part( "Page-Templates/Logo/index.htm" ); ?>
