38f747659929769ce052f5f8afbca05c9d8025c6
Tester <seumworldinbangladesh@gmail.com>
2021-04-20 08:23:17 +0600

    add wp styles form

--- testmenupage.php
+++ testmenupage.php
@@ -1,7 +1,12 @@
 <?php function ansome() {
     ?>
 
-<h1>SALAM</h1>
+<h1 style="color:#242523">SALAM</h1>
+<input 
+id="community-events-location" 
+class="regular-text" type="text" 
+name="community-events-location" 
+placeholder=" Awesome ">
 <?php
 
 } 
