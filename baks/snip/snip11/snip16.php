96d2be7567dfa5aa28dc4a51eaa7bc1982032c44
Tester <seumworldinbangladesh@gmail.com>
2021-04-20 12:09:13 +0600

    get post data extarction done

---
+++ baks/snip/snip11/snip16.php
@@ -0,0 +1,9 @@
+<?php //var_dump(get_post(108));  ?>
+<?php //get_post(108); ?>
+<?php print_r(get_post([108 => 'post_content'])); ?>
+<?php _e(get_post(108,post_content())); ?>
+<?php $post_7 = get_post( 108 ); 
+$title = $post_7->post_title; ?>
+<div id="boxmID">
+<div class="welcome-panel"> </div>
+ </div>
