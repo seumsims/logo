3954be58feac4f88e62dd0899577aafbffd9109c
Tester <seumworldinbangladesh@gmail.com>
2021-04-20 08:39:15 +0600

    so we need use of function

--- testmenupage.php
+++ testmenupage.php
@@ -1,18 +1,1 @@
-<?php wp_nav_menu( 'Primary menu logo' );
- function ansome() {
-    ?>
-
-<h1 style="color:#242523">SALAM</h1>
-<input 
-id="community-events-location" 
-class="regular-text" type="text" 
-name="community-events-location" 
-placeholder=" Awesome ">
-<textarea id="metavalue" name="metavalue" rows="2" cols="25"></textarea>
-<?php
-
-} 
-ansome();
-?>
-
-
+<?php 
