2a50d0acbfeef2023a6254b338383f70ad7e05d8
Tester <seumworldinbangladesh@gmail.com>
2021-04-20 13:41:58 +0600

    again post ouput getting

--- testmenupage.php
+++ testmenupage.php
@@ -1,6 +1,12 @@
 <?php   ?>
 <div id="boxmID">
 <div class="welcome-panel"> 
+<?php 
 
+$post   = get_post( 405 );
+$output =  apply_filters( 'the_content', $post->post_content );
+_e($output);
+
+?>
 </div>
 </div>
