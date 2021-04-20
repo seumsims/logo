1ace14a351633443166e84b6d856b6f51bd3c331
Tester <seumworldinbangladesh@gmail.com>
2021-04-21 00:50:37 +0600

    some trims

--- baks/snip/snip11/snip22.php
+++ baks/snip/snip11/snip22.php
@@ -6,13 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo( 'name' ); ?></title>
-    <?php wp_head(); ?>
+    <?php wp_head(  ); ?>
 </head>
-<body <?php body_class(  ); ?>>
-
-<?php get_template_part( "templates/banner" ); ?>   
-
-
-<?php wp_footer(); ?>   
+<body <?php body_class(  )?>>
+<?php 
+//esc_html_e( basename(get_page_template(  ))  );
+?>
+<?php //the_title( ); ?>
+<?php the_content(); ?>
+<?php wp_footer(  ) ?>   
 </body>
 </html>
---
+++ templates/banner.php
@@ -0,0 +1,31 @@
+ <!-- bannar start  -->
+ <section class="bannar-wrapper overflow-hidden" >
+            <div class="bannar-active" >
+                <div class="bannar-bg-img bannar-height " data-background="./assets/image/bannar/bannar-bg.jpg">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xl-8">
+                                <div class="bannar-content">
+                                    <span>Get Every Single Solutions.</span>
+                                    <h1>I'm <span>a web designer</span></h1>
+                                    <p>I'm a Freelance UI/UX Designer and Developer based in London, England. I strives to build immersive and beautiful web applications through carefully crafted code and user-centric design.</p>
+                                    <div class="btn-group btn-two">
+                                        <a href="#" class="btn"><i class="fas fa-user"></i>more about me</a>
+                                        <a href="#" class="btn"><i class="fas fa-link"></i>prothfolio </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class=" bannar-person_image">
+                        <div class="t-slider-img"> </div>
+                        
+                   <div class="bannar-person">
+                    <img src="./assets/image/bannar/man-sheap.png" alt="">
+                </div>
+                    </div>
+                </div>
+ 
+            </div>
+        </section>
+        <!-- bannar end  -->
