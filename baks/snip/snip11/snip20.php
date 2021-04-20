c0b1149b4ab3238b606fc2a7d37e2e4f2158ffd6
Tester <seumworldinbangladesh@gmail.com>
2021-04-20 12:29:01 +0600

    add post and text area both

--- baks/snip/snip11/snip20.php
+++ baks/snip/snip11/snip20.php
@@ -11,22 +11,13 @@ $postlogo_content = $postlogo->post_content;
 _e($postlogo_content);
 
 ?>
-<label for="w3review">Edit Logo:</label>
-
-<textarea id="w3review" name="w3review" rows="15" cols="150">
-<?php 
-
-$postlogo = get_post( 405 ); 
-$postlogo_content = $postlogo->post_content; 
-_e($postlogo_content);
-
-?>
-</textarea>
 </div>
 
+<label for="w3review">Review of W3Schools:</label>
 
-
-
+<textarea id="w3review" name="w3review" rows="4" cols="50">
+At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.
+</textarea>
 
  </div>
  </div>
