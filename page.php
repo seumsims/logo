f4b63d600792557ab333520e1d0c63cc1e828232
Tester <seumworldinbangladesh@gmail.com>
2021-04-21 09:59:01 +0600

    page set done

---
+++ page.php
@@ -0,0 +1,12 @@
+<?php get_header( ); ?>  
+
+<body <?php body_class(  ); ?>>
+
+<?php get_template_part( "templates/top-menu-and-top-bar-section" ); ?>   
+
+<?php the_content(); ?> 
+
+  
+<?php get_footer(); ?>   
+</body>
+</html>
