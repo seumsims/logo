61ff93c70c9a3baac5f4ce269fdc5bcb16883d58
Tester <seumworldinbangladesh@gmail.com>
2021-04-23 20:38:42 +0600

    refactored again

--- single.php
+++ single.php
@@ -1,1 +1,18 @@
-<?php
+<?php get_header( ); ?>  
+
+<body <?php body_class(  ); ?>>
+
+<?php get_template_part( "templates/top-menu-and-top-bar-section" ); ?>   
+<?php 
+while(have_posts(  )) {
+    $logo_post_start_text = "<span" . " class" . "= " . " logo-post-start-text " . ">" . ' start ' . "</span>" ;
+    the_post();
+    the_title( $logo_post_start_text   ,' end '  );
+    the_content(  );
+}
+ ?>   
+
+ 
+
+
+<?php get_footer(); ?>   
