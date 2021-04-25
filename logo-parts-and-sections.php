c859073c0371f2e61bf607030b62f888591852e8
Tester <seumworldinbangladesh@gmail.com>
2021-04-25 17:25:07 +0600

    retrive post id data from id works but esc html e escape it

--- logo-parts-and-sections.php
+++ logo-parts-and-sections.php
@@ -11,7 +11,7 @@ Template Name: Logo Sections
 <?php get_template_part( "templates/top-menu-and-top-bar-section" ); ?>   
 <?php get_template_part( "templates/banner" ); ?>   
 <?php 
-$post   = get_post( 403 );
+$post   = get_post( 534 );
 $output =  apply_filters( 'the_content', $post->post_content ); 
 esc_html_e( $output , 'logo' );
 ?>   
