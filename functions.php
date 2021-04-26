7af62fb2703e9225ff197142b752f89c45035265
Tester <seumworldinbangladesh@gmail.com>
2021-04-26 11:55:21 +0600

    refactored

--- functions.php
+++ functions.php
@@ -78,6 +78,21 @@ function logo_bootstrapping() {
             )
         );
 
+    add_filter('manage_posts_columns', 'posts_columns_id', 5);
+    add_action('manage_posts_custom_column', 'posts_custom_id_columns', 5, 2);
+    add_filter('manage_pages_columns', 'posts_columns_id', 5);
+    add_action('manage_pages_custom_column', 'posts_custom_id_columns', 5, 2);
+ 
+function posts_columns_id($defaults){
+    $defaults['wps_post_id'] = __('ID');
+    return $defaults;
+}
+function posts_custom_id_columns($column_name, $id){
+    if($column_name === 'wps_post_id'){
+            echo $id;
+    }
+}
+
 }
 add_action("after_setup_theme","logo_bootstrapping");
 
@@ -161,17 +176,4 @@ function logo_index_page_template_banner() {
 
 add_action( "wp_head", "logo_index_page_template_banner",100 );
 
-add_filter('manage_posts_columns', 'posts_columns_id', 5);
-    add_action('manage_posts_custom_column', 'posts_custom_id_columns', 5, 2);
-    add_filter('manage_pages_columns', 'posts_columns_id', 5);
-    add_action('manage_pages_custom_column', 'posts_custom_id_columns', 5, 2);
- 
-function posts_columns_id($defaults){
-    $defaults['wps_post_id'] = __('ID');
-    return $defaults;
-}
-function posts_custom_id_columns($column_name, $id){
-    if($column_name === 'wps_post_id'){
-            echo $id;
-    }
-}
+
