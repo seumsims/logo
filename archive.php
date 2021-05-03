3dea2c550ebd9ffdb185c4c29c0baff51e3d3915
Tester <seumworldinbangladesh@gmail.com>
2021-05-03 13:01:56 +0600

    get the permalink and sprintf not satisfactory

--- archive.php
+++ archive.php
@@ -5,7 +5,12 @@
 
 while ( have_posts() ) :
     the_post();
-    the_excerpt( the_permalink(  ) );
+    the_excerpt(  );
+
+    sprintf( '<a class="read-more" href="%1$s">%2$s</a>',
+            get_permalink( get_the_ID() ),
+            __( 'Read More', 'logo' )
+);
 
  
 
