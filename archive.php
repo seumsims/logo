c9f324e2c0e2d6bac2712acdfc811454857f0e3f
Tester <seumworldinbangladesh@gmail.com>
2021-05-03 13:44:24 +0600

    some cleans on posts

--- archive.php
+++ archive.php
@@ -1,22 +1,28 @@
 <?php get_header( ); ?>  
 <?php if ( have_posts() ) : ?>
-
+<div <?php post_class( array('logo-post-permalink-link-color-cls-id-1') ); ?>>
 <?php
 
 while ( have_posts() ) :
     the_post();
+    the_title(  );
     the_excerpt(  );
+    ?> 
 
-    sprintf( '<a class="read-more" href="%1$s">%2$s</a>',
-            get_permalink( get_the_ID() ),
-            __( 'Read More', 'logo' )
-);
-
+    <div class="">
+    
+    <span>
+        <a href="<?php  the_permalink(); ?>">Read More</a>
+    </span>
+    
+    </div>
+     <?php
  
 
 endwhile;
 endif;
 ?>
-
+</div>
+<!-- Posts End -->
 
 <?php get_footer(); ?>   
