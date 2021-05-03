a3a96ac8afae12c66b5fed5cfdae5c3652f71156
Tester <seumworldinbangladesh@gmail.com>
2021-05-03 22:33:14 +0600

    c widget working

--- functions.php
+++ functions.php
@@ -6,13 +6,15 @@
 
 function logo_bootstrapping() {
 
-    require_once("required/customizer/kirki/class-kirki-preset.php");
+require_once("required/customizer/kirki/class-kirki-preset.php");
 require_once("required/customizer/kirki/kirki-mod.php");
 require_once("required/customizer/logo-banner-customizer-preset.php");
 
+
     
 
-    load_theme_textdomain("logo");
+    
+load_theme_textdomain("logo");
     add_theme_support("post-thumbnails");
     add_theme_support("title-tag");
     add_theme_support( "custom-header" );
@@ -93,13 +95,16 @@ require_once("required/customizer/logo-banner-customizer-preset.php");
             )
         );
 
+
+        
+
     
 
 }
 add_action("after_setup_theme","logo_bootstrapping");
 
 
-
+//require_once("required/classes/widget-classes/logo-test-widget-class.php");
 
 
 function logo_styles() {
@@ -144,6 +149,85 @@ function logo_scripts() {
     
 }
 add_action("wp_enqueue_scripts", "logo_scripts");
+
+function arphabet_widgets_init() {
+
+	register_sidebar( array(
+		'name'          => 'Home right sidebar',
+		'id'            => 'home_right_1',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="rounded">',
+		'after_title'   => '</h2>',
+	) );
+
+}
+add_action( 'widgets_init', 'arphabet_widgets_init' );
+// Creating the widget 
+class wpb_widget extends WP_Widget {
+  
+    function __construct() {
+    parent::__construct(
+      
+    // Base ID of your widget
+    'wpb_widget', 
+      
+    // Widget name will appear in UI
+    __('WPBeginner Widget', 'wpb_widget_domain'), 
+      
+    // Widget description
+    array( 'description' => __( 'Sample widget based on WPBeginner Tutorial', 'wpb_widget_domain' ), ) 
+    );
+    }
+      
+    // Creating widget front-end
+      
+    public function widget( $args, $instance ) {
+    $title = apply_filters( 'widget_title', $instance['title'] );
+      
+    // before and after widget arguments are defined by themes
+    echo $args['before_widget'];
+    if ( ! empty( $title ) )
+    echo $args['before_title'] . $title . $args['after_title'];
+      
+    // This is where you run the code and display the output
+    echo __( 'Hello, World!', 'wpb_widget_domain' );
+    echo $args['after_widget'];
+    }
+              
+    // Widget Backend 
+    public function form( $instance ) {
+    if ( isset( $instance[ 'title' ] ) ) {
+    $title = $instance[ 'title' ];
+    }
+    else {
+    $title = __( 'New title', 'wpb_widget_domain' );
+    }
+    // Widget admin form
+    ?>
+    <p>
+    <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+    <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+    </p>
+    <?php 
+    }
+          
+    // Updating widget replacing old instances with new
+    public function update( $new_instance, $old_instance ) {
+    $instance = array();
+    $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+    return $instance;
+    }
+     
+    // Class wpb_widget ends here
+    } 
+     
+     
+    // Register and load the widget
+    function wpb_load_widget() {
+        register_widget( 'wpb_widget' );
+    }
+    add_action( 'widgets_init', 'wpb_load_widget' );
 
 
 
